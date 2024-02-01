462201694f03014d61bdfe0f09e72f07f1e33baa
Uditya-Singh007 <119700075+Uditya-Singh007@users.noreply.github.com>
2024-02-02 02:35:30 +0530

    nine1

--- 9.js
+++ 9.js
@@ -1,7 +1,7 @@
 const a = [1,2,999,56,"Mithun",1234,"PW"];
 
-for (let i=0;i<a.length;i++){
-  if (typeo a[i]==="string"){
+for (let i =0;i<a.length;i++){
+  if (typeof a[i] === "string") {
     console.log(`Found The First String: ${a[i]}`);
     break;
   }
